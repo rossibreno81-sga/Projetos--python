0208564a27fc955fc602859ad47d83116b6794e5
rossibreno81-sga <rossibreno81@gmail.com>
2026-05-04 09:43:59 -0300

    v1

---
+++ Seletor.py
@@ -0,0 +1,9 @@
+#--Seletor de Caracteres
+letra = input("Digite a letra")
+if letra == "A":
+  print("Você escolheu a letra A")
+elif letra == "B"
+  print("Você escolheu a letra B")
+else:
+  print("Essa opção não esta disponivel")
+  
