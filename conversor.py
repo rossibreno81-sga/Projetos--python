9a69505f271bee4427cc6f43367b731d44174fbc
rossibreno81-sga <rossibreno81@gmail.com>
2026-05-25 12:51:33 +0000

    v1

---
+++ conversor.py
@@ -0,0 +1,10 @@
+#Ferramenta de Conversão dólar x real--
+def converter(valor_dolar):
+    taxa = 5.15
+    valor_real = valor_dolar * taxa
+    return valor_real
+print("Conversor dólar x real")
+preço = float(input("Digite o preço do produto em Dólar:"))
+resultado = converter(preco)
+print(f"O valor em reais é: {resultdo:.2f}")
+              
