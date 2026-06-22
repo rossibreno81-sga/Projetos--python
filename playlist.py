3f3f0b11307981f38a4f5730f9da90367892d3de
rossibreno81-sga <rossibreno81@gmail.com>
2026-06-22 12:41:05 +0000

    v1

---
+++ playlist.py
@@ -0,0 +1,6 @@
+print("Music Player")
+Playlist = ["Musica 1-rock", "Musica 2-POP", "Musica 3-RAP"]
+atual = 0
+print(f"Tocando Agora: {playlist[atual]}")
+print("Musica Anterior:--(inicio da playlist)")
+print("=" *40)
