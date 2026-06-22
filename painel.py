f01cd3748749fb15f34758e6a68808727f53a20a
rossibreno81-sga <rossibreno81@gmail.com>
2026-06-22 12:30:38 +0000

    v1

---
+++ painel.py
@@ -0,0 +1,7 @@
+print("--- PAINEL DE ATENDIMENTO---")
+fita_espera = ["senha 01", "senha 02", "senha 03", "senha 04"]
+print(f"o proximo paciente a ser atendido é: {fila_espera[01]}")
+print(f"o segundo paciente a ser atendido é: {fila_espera[02]}")
+
+total = len(fila_espera)
+print(f"total aguardando: {total}")
